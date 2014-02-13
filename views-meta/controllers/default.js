76d12b0e51acbc2e1ce682537636b1d6c78d18b2
Peter Širka <petersirka@gmail.com>
2014-02-14 00:05:05 +0100

    updated: example

--- views-meta/controllers/default.js
+++ views-meta/controllers/default.js
@@ -1,6 +1,8 @@
 exports.install = function(framework) {
 	framework.route('/', view_homepage);
 	framework.route('/controller/', view_homepage_controller);
+	framework.route('/custom/', view_custom);
+	framework.route('/controller/custom/', view_custom_controller);
 };
 
 function view_homepage() {
@@ -8,25 +10,29 @@ function view_homepage() {
 	self.view('homepage');
 }
 
+function view_custom() {
+	var self = this;
+	self.view('custom');
+}
+
 function view_homepage_controller() {
 	var self = this;
-	
+
 	self.meta('title controller', 'description controller', 'keywords controller');
-	
-	// self.meta('title controller', 'description controller', 'meta image_src');
-	//
-	// you can write own meta render
-	//
-	// framework.onMeta = function() {
-	//    arguments.forEach(function(o) {
-	//       ...
-	//	  });
-	//    return '<title>HA HA HA</title>';
-	// };
-	//
+
+	// self.meta('title controller', 'description controller', 'keywords', 'meta image_src');
+
 	// ===========================================================================
 	// look at example: framework-custom
 	// ===========================================================================
 
 	self.view('homepage-controller');
+}
+
+function view_custom_controller() {
+	var self = this;
+	self.title('title controller');
+	self.description('description controller');
+	self.keywords('keywords controller');
+	self.view('custom-controller');
 }
