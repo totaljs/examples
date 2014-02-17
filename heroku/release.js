c9fcc4053102774ae34d486d67a2263b39382b26
Peter Širka <petersirka@gmail.com>
2014-02-17 01:24:20 +0100

    updated: example

--- heroku/release.js
+++ heroku/release.js
@@ -2,4 +2,4 @@ var framework = require('total.js');
 var http = require('http');
 
 // Debug mode = false
-framework.run(http, false, parseInt(process.env.PORT || 5000), 'heroku');
+framework.run(http, false);
