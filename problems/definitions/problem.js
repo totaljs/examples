39f5f99d5a3b5e294d456c0924f11c296704bb1e
Peter Širka <petersirka@gmail.com>
2014-03-01 12:22:56 +0100

    updated: event

--- problems/definitions/problem.js
+++ problems/definitions/problem.js
@@ -1,12 +1,13 @@
 
 // Problem output
 
-framework.on('problem', function(message, name, uri) {
+framework.on('problem', function(message, name, uri, ip) {
 	
 	console.log('PROBLEM:');
 	console.log('message:', message);
 	console.log('name:', name);
 	console.log('uri:', uri.href);
+	console.log('ip:', ip);
 	console.log('');
 
 });
