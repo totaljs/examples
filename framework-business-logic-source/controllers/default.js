30a6125f8d66843e6cc631b1723132cef784a29d
Peter Širka <petersirka@gmail.com>
2014-05-08 21:43:37 +0200

    updated: comments

--- framework-business-logic-source/controllers/default.js
+++ framework-business-logic-source/controllers/default.js
@@ -8,8 +8,13 @@ function view_homepage() {
 
 	// This function does the same as require (only adds path to the source directory)
 
-	var layer_include = include('layer');
-	var layer_source = source('layer');
+    // INCLUDE == SOURCE are same
+
+    // http://docs.totaljs.com/global/#INCLUDE
+	var layer_include = INCLUDE('layer');
+
+    // http://docs.totaljs.com/global/#SOURCE
+	var layer_source = SOURCE('layer');
 
 	self.plain('{0}\n{1}'.format(layer_include.hello(), layer_source.hello()));
 }
