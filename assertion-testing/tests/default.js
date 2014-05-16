5adee6a440ab193c767b08656fbef61d2938198a
Peter Širka <petersirka@gmail.com>
2014-05-16 16:03:33 +0200

    fixed: code

--- assertion-testing/tests/default.js
+++ assertion-testing/tests/default.js
@@ -2,27 +2,28 @@ var assert = require('assert');
 
 exports.run = function(framework, name) {
 
-	console.log('run test: {0}'.format(name));
-	console.log('');
+	/**
+     * @see {@link http://docs.totaljs.com/Framework/#framework.assert|Documentation}
+	 */
 
-	// Documentation: http://docs.totaljs.com/Framework/#framework.assert
-	
-	framework.assert('Test methods', function(name) {		
-		assert.ok(framework.controller('default').functions.increment(1) === 2, name + ' default controller: increment()');
+	framework.assert('controller.increment()', function(next, name) {
+		assert.ok(framework.controller('default').functions.increment(1) === 2, name);
+		next();
 	});
-	
-	framework.assert('Test URL 1', '/1/', function(error, data, name, code, headers) {
+
+	framework.assert('Test URL 1', '/1/', ['GET'], function(error, data, code, headers, cookies, name) {
+		console.log(data, code)
 		assert.ok(code === 200 && data === '1', name);
 		console.log('1');
 	});
 
-	framework.assert('Test URL 2', '/2/', function(error, data, name, code, headers) {
+	framework.assert('Test URL 2', '/2/', ['GET'], function(error, data, code, headers, cookies, name) {
 		assert.ok(code === 200 && data === '2', name);
 		console.log('2');
 	});
 
-	framework.assert('Test URL 3', '/3/', function(error, data, name, code, headers) {
-		// throw error, data === 4
+	framework.assert('Test URL 3', '/3/', ['GET'], function(error, data, code, headers, cookies, name) {
+		// throws error, data === 4
 		assert.ok(code === 200 && data === '3', name);
 		console.log('3');
 	});
