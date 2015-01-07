34f9f3a7f533213c04cc25fdd39ec397d17cc2dd
Peter Širka <petersirka@gmail.com>
2015-01-07 12:07:33 +0100

    fixed: example

--- external-compile-sass/definitions/sass.js
+++ external-compile-sass/definitions/sass.js
@@ -2,5 +2,5 @@ var sass = require('node-sass');
 
 framework.onCompileStyle = function (filename, content) {
 	// if filename === '' then it is the inline style
-	return sass.renderSync({ data: content, outputStyle: 'compressed' });
+    return sass.renderSync({ data: content, outputStyle: 'compressed' }).css;
 };
