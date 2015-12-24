4c5881ab93c8aa81cfec8e0f7af9c901b87424cc
Peter Širka <petersirka@gmail.com>
2015-12-24 14:12:07 +0100

    New changes.

---
+++ themes/themes/red/index.js
@@ -0,0 +1,13 @@
+// This is the initialization script for this theme
+// Is optional
+exports.install = function() {
+
+	// Custom mapping
+	F.map('/red-theme-style.css', '=red/public/css/default.css');
+	// Try: http://127.0.0.1:8000/red-theme-style.css
+
+	console.log('RED THEME IS INITIALIZED');
+
+	// =red --> is a shortcut and the framework compiles it as `/app/themes/red/` path.
+	// F.merge() is same as F.map()
+};
