9146422f4d8c07766e4e2563c29240e71ac2502a
Peter Širka <petersirka@gmail.com>
2016-08-12 13:48:34 +0200

    Shows sent data.

--- validation/models/contact.js
+++ validation/models/contact.js
@@ -6,16 +6,20 @@ NEWSCHEMA('Contact').make(function(schema) {
 	schema.define('age', 'Number', true);
 	schema.define('terms', 'Boolean', true);
 
+	// Sets prefix into the resource file
 	schema.setPrefix('error-');
 
 	schema.setValidate(function(name, value) {
 		switch (name) {
 			case 'age':
+				// We want custom error message
 				return value < 17 ? 'You are too young.' : value > 40 ? 'You are too old' : true;
 		}
 	});
 
 	schema.setSave(function(error, model, options, callback) {
+		var data = model.$plain();
+		console.log('NEW CONTACT FORM:', data);
 		callback(SUCCESS(true));
 	});
 
