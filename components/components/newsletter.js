a77c2196a3e807d83e1796c34e40955323c25a8d
Peter Širka <petersirka@gmail.com>
2016-12-16 11:08:51 +0100

    Update `components` example.

---
+++ components/components/newsletter.js
@@ -0,0 +1,13 @@
+// This is server-side implementaion of component
+
+exports.install = function() {
+	F.route('/api/newsletter/', json_newsletter, ['post']);
+};
+
+function json_newsletter() {
+	var self = this;
+	self.body.ip = self.ip;
+	self.body.created = F.datetime;
+	NOSQL('newsletter').upsert(self.body);
+	self.json(SUCCESS(true));
+}
