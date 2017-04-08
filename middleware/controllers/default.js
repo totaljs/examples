b6ef7c6ab3c2fe16e3ed017a8b7930f9f148f000
Peter Širka <petersirka@gmail.com>
2017-04-08 17:33:22 +0200

    Improve code.

--- middleware/controllers/default.js
+++ middleware/controllers/default.js
@@ -3,7 +3,7 @@ exports.install = function() {
 	F.route('/b/', view, ['#B']);
 	F.route('/c/', view, ['#C']); // if flag starts with # then is registered as middleware
 	F.route('/all/', view, ['#A', '#B', '#C']);
-}
+};
 
 function view() {
 	var self = this;
