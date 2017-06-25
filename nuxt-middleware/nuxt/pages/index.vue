85a5b32d587c32093232a5790fadb9a01c9e6492
ckpiggy <mrgmp2004@hotmail.com>
2017-06-25 14:12:02 +0800

    add vuex to enable react values

--- nuxt-middleware/nuxt/pages/index.vue
+++ nuxt-middleware/nuxt/pages/index.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
         <h3>Hello from Total.js and Nuxt.js</h3>
+        <div class="react-container">
+            <p>These values are React</p>
+            <input :value="msg" @input="(e)=>{setMsg(e.target.value)}"/>
+            <p>{{msg}}</p>
+            <p>counter: {{counter}}</p>
+            <button @click="decrease">-</button><button @click="increase">+</button>
+        </div>
     </div>
 </template>
 <script>
-
+    import {mapState, mapMutations} from 'vuex'
+    export default {
+      computed: {
+        ...mapState(['msg', 'counter'])
+      },
+      methods: {
+        ...mapMutations(['setMsg', 'increase', 'decrease'])
+      }
+    }
 </script>
 <style scoped>
-
+    .react-container {
+        padding: 0 20px 20px 20px;
+        margin: 10px;
+        border-color: darkgray;
+        border-style: solid;
+        border-radius: 5px;
+        border-width: 1px;
+        display: inline-block;
+        width: 50%;
+    }
 </style>
