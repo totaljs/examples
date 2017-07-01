1aa07c7dec885c48de9ad030d1aa258dd79009c3
Peter Širka <petersirka@gmail.com>
2017-07-01 17:40:43 +0200

    Remove bad code.

--- assertion-testing/tests/default.js
+++ assertion-testing/tests/default.js
@@ -5,20 +5,20 @@ TEST('controller.increment()', function() {
 TEST('Test URL 1', '/1/', function(builder) {
 	builder.get();
 	builder.exec(function(err, response, output) {
-		FAIL(output.response !== '1', name);
+		FAIL(output.response !== '1');
 	});
 });
 
 TEST('Test URL 2', '/2/', function(builder) {
 	builder.get();
 	builder.exec(function(err, response, output) {
-		FAIL(output.response !== '2', name);
+		FAIL(output.response !== '2');
 	});
 });
 
 TEST('Test URL 3', '/3/', function(builder) {
 	builder.json({ data: 4 });
 	builder.exec(function(err, response) {
-		FAIL(response.data !== 3, name);
+		FAIL(response.data !== 3);
 	});
 });
