3e39ba27b37f809b3e8652c7456d34c7ee539a7f
Peter Širka <petersirka@gmail.com>
2017-07-01 17:33:07 +0200

    Update example.

---
+++ assertion-testing/readme.md
@@ -0,0 +1,4 @@
+# Unit-Testing
+
+- Total.js `+v2.7.0`
+- run `$ node test.js`
--- assertion-testing/tests/default.js
+++ assertion-testing/tests/default.js
@@ -1,21 +1,24 @@
-exports.run = function() {
+TEST('controller.increment()', function() {
+	FAIL(F.controller('default').functions.increment(1) !== 2);
+});
 
-	F.assert('controller.increment()', function(next, name) {
-		assert.ok(F.controller('default').functions.increment(1) === 2, name);
-		next();
+TEST('Test URL 1', '/1/', function(builder) {
+	builder.get();
+	builder.exec(function(err, response, output) {
+		FAIL(output.response !== '1', name);
 	});
+});
 
-	F.assert('Test URL 1', '/1/', ['get'], function(error, data, code, headers, cookies, name) {
-		assert.ok(code === 200 && data === '1', name);
+TEST('Test URL 2', '/2/', function(builder) {
+	builder.get();
+	builder.exec(function(err, response, output) {
+		FAIL(output.response !== '2', name);
 	});
+});
 
-	F.assert('Test URL 2', '/2/', ['get'], function(error, data, code, headers, cookies, name) {
-		assert.ok(code === 200 && data === '2', name);
+TEST('Test URL 3', '/3/', function(builder) {
+	builder.json({ data: 4 });
+	builder.exec(function(err, response) {
+		FAIL(response.data !== 3, name);
 	});
-
-	F.assert('Test URL 3', '/3/', ['post', 'json'], function(error, data, code, headers, cookies, name) {
-		// throws error, data === 4
-		assert.ok(code === 200 && JSON.parse(data).data === 3, name);
-	}, { data: 4 });
-
-};
+});
