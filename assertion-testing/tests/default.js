c4aa5279c17ef42c5e14d2e013b8dd24e08d7e74
Peter Širka <petersirka@gmail.com>
2017-07-03 11:07:51 +0200

    Update test.

--- assertion-testing/tests/default.js
+++ assertion-testing/tests/default.js
@@ -3,6 +3,7 @@ TEST('controller.increment()', function() {
 });
 
 TEST('Test URL 1', '/1/', function(builder) {
+    // builder === RESTBuilder
 	builder.get();
 	builder.exec(function(err, response, output) {
 		FAIL(output.response !== '1');
@@ -10,13 +11,16 @@ TEST('Test URL 1', '/1/', function(builder) {
 });
 
 TEST('Test URL 2', '/2/', function(builder) {
+    // builder === RESTBuilder
 	builder.get();
 	builder.exec(function(err, response, output) {
-		FAIL(output.response !== '2');
+		FAIL(output.response !== '2', 'additional description');
+		OK(output.status === 200, 'HTTP status');
 	});
 });
 
 TEST('Test URL 3', '/3/', function(builder) {
+    // builder === RESTBuilder
 	builder.json({ data: 4 });
 	builder.exec(function(err, response) {
 		FAIL(response.data !== 3);
