963d0bbb0981a6fb63a7215759e31ae942277dec
Peter Širka <petersirka@gmail.com>
2017-12-07 22:09:50 +0100

    Fix loading connection.

--- mongodb/definitions/database.js
+++ mongodb/definitions/database.js
@@ -1,10 +1,13 @@
 var MC = require('mongodb').MongoClient;
 var DB = null;
 
+F.wait('database');
+
 MC.connect(CONFIG('database'), function(err, db) {
 	if (err)
 		throw err;
 	DB = db;
+	F.wait('database');
 });
 
 F.database = function(collection) {
