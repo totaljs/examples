50199316ff4ac07f1e42904841c11030af0eff73
Peter Širka <petersirka@gmail.com>
2017-12-19 17:14:32 +0100

    Fix test.

--- config-debug-release-test/tests/config.js
+++ config-debug-release-test/tests/config.js
@@ -1,11 +1,13 @@
-exports.run = function() {
-    var builder = [];
+TEST('config', function() {
+	var builder = [];
 
-    Object.keys(F.config).forEach(function(o) {
-        var value = F.config[o];
-        builder.push('{0} : {1}'.format(o.padRight(30, ' '), value instanceof Array ? value.join(', ') : value));
-    });
+	Object.keys(F.config).forEach(function(o) {
+		var value = F.config[o];
+		builder.push('{0} : {1}'.format(o.padRight(30, ' '), value instanceof Array ? value.join(', ') : value));
+	});
 
-    console.log(builder.join('\n'));
-    console.log('');
-};
+	console.log(builder.join('\n'));
+	console.log('');
+
+	OK(true);
+});
