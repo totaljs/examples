a1d89fc9fa4edc80b8ea51354e442f51260d215b
Peter Sirka <petersirka@gmail.com>
2020-05-08 11:23:08 +0200

    Added new example.

---
+++ remote-terminal/controllers/default.js
@@ -0,0 +1,47 @@
+const Pty = require('node-pty');
+
+exports.install = function() {
+
+	// Route to views/index
+	ROUTE('/');
+
+	// WebSocket route
+	WEBSOCKET('/', socket, ['raw']);
+
+};
+
+function socket() {
+
+	var self = this;
+
+	self.encodedecode = false;
+	self.autodestroy();
+
+	self.on('open', function(client) {
+
+		// Each client will have own terminal
+		client.tty = Pty.spawn('/bin/bash', [], { name: 'xterm-color', cols: 80, rows: 24, cwd: process.env.PWD, env: process.env });
+
+		client.tty.on('exit', function(code, signal) {
+			// What now?
+			client.tty = null;
+			client.close();
+		});
+
+		client.tty.on('data', function(data) {
+			client.send(data);
+		});
+
+	});
+
+	self.on('close', function(client) {
+		if (client.tty) {
+			client.tty.kill(9);
+			client.tty = null;
+		}
+	});
+
+	self.on('message', function(client, msg) {
+		client.tty && client.tty.write(msg);
+	});
+}
---
+++ remote-terminal/readme.md
@@ -0,0 +1,8 @@
+# Remote terminal
+
+```
+$ npm install
+$ node index.js
+```
+
+Then open web browser `http://127.0.0.1:8000`
