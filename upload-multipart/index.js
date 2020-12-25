fed0209e37810e792f4f78e4c26b76dbc9a1741e
Peter Sirka <petersirka@gmail.com>
2020-12-25 23:44:33 +0100

    New updates.

--- upload-multipart/index.js
+++ upload-multipart/index.js
@@ -1,6 +1,5 @@
 // ===================================================
-// FOR DEVELOPMENT
-// Total.js - framework for Node.js platform
+// Total.js start script
 // https://www.totaljs.com
 // ===================================================
 
@@ -8,9 +7,23 @@ const options = {};
 
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
+// options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 // options.inspector = 9229;
-// options.debugger = 40894;
+// options.watch = ['private'];
+// options.livereload = 'https://yourhostname';
 
-require('total.js/debug')(options);
+// Enables cluster:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
+
+// Enables threads:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
+// options.timeout = 5000;
+// options.threads = '/api/';
+// options.logs = 'isolated';
+
+var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+require('total4/' + type)(options);
---
+++ validation/schemas/contacts.js
@@ -0,0 +1,16 @@
+NEWSCHEMA('Contacts', function(schema) {
+
+	schema.define('firstname', 'Name(30)', true);
+	schema.define('lastname', 'Name(30)', true);
+	schema.define('email', 'Email', true);
+	schema.define('age', Number, age => age < 18 ? 'You are too young.' : age > 40 ? 'You are too old' : true);
+	schema.define('terms', Boolean, true);
+
+	schema.setSave(function($, model) {
+		model.id = UID();
+		model.dtcreated = NOW;
+		delete model.terms;
+		NOSQL('contacts').insert(model).callback($.done());
+	});
+
+});
