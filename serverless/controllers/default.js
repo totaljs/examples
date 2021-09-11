0ff034c352dfb9dc3b4fe084ff7498ebcd82ac3d
Peter Širka <petersirka@gmail.com>
2021-09-11 08:24:37 +0200

    Fixed indentation.

--- serverless/controllers/default.js
+++ serverless/controllers/default.js
@@ -1,14 +1,15 @@
 exports.install = function () {
-    ROUTE('GET /', function () {
-        var self = this;
-        self.plain('REST Service {0}\nVersion: {1}'.format(CONF.name, CONF.version));
-    })
 
-    // Sets cors for the entire API
-    CORS();
-    ROUTE('GET    /api/users/        *Users --> query');
-    ROUTE('GET    /api/users/{id}/   *Users --> read');
-    ROUTE('POST   /api/users/        *Users --> insert');
-    ROUTE('PUT    /api/users/{id}/   *Users --> update');
-    ROUTE('DELETE /api/users/{id}/   *Users --> remove');
-}
+	ROUTE('GET /', function () {
+		var self = this;
+		self.plain('REST Service {0}\nVersion: {1}'.format(CONF.name, CONF.version));
+	});
+
+	// Sets cors for the entire API
+	CORS();
+	ROUTE('GET    /api/users/        *Users --> query');
+	ROUTE('GET    /api/users/{id}/   *Users --> read');
+	ROUTE('POST   /api/users/        *Users --> insert');
+	ROUTE('PUT    /api/users/{id}/   *Users --> update');
+	ROUTE('DELETE /api/users/{id}/   *Users --> remove');
+};
