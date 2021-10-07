92cbd7a276e931af0644c8c3a19e444ca563cadd
Peter Širka <petersirka@gmail.com>
2021-10-07 08:58:53 +0200

    Updated example.

--- server-sent-events/controllers/default.js
+++ server-sent-events/controllers/default.js
@@ -1,48 +1,30 @@
 exports.install = function() {
 	ROUTE('GET /');
-	ROUTE('GET /one-way-messaging/', view_SSE, ['sse']);
+	ROUTE('GET /sse/', serversentevents, ['sse']);
 };
 
-function view_SSE() {
+function serversentevents() {
 
 	var self = this;
+	var lasteventid = self.sseID || '0';
+	var counter = U.parseInt(lasteventid);
 	var indexer = 0;
-	var counter = parseInt(self.sseID || '0');
-
-	console.log('Last event ID:', self.sseID);
 
 	var interval = setInterval(function() {
 
-		if (!self.isConnected) {
-			clearInterval(interval);
-			return;
-		}
-
-		/*
-			Send data to client - One way messaging
-
-			@data {String or Object}
-			@eventname {String} :: default null
-			@id {String} :: default null (Last Event ID)
-			@retry {Number} :: in milliseconds, default CONF.default_request_timeout
-
-			controller.sse(data, [eventname], [id], [retry])
-		*/
-		self.sse({ counter: counter++, message: GUID(10) }, null, counter);
-
-		// reconnect by the @retry
-		indexer++;
-		if (indexer > 10) {
-			clearInterval(interval);
+		// Closes client after 10 seconds
+		if (indexer++ > 10) {
 			self.close();
+			clearInterval(interval);
+		} else {
+			// Sends data to the client
+			if (!self.sse({ counter: counter++, message: GUID() }, null, counter)) {
+				// Something wrong
+				clearInterval(interval);
+			}
 		}
 
 	}, 1000);
 
-	/*
-	self.res.on('close', function() {
-		console.log('CLIENT DISCONNECT');
-	});
-	*/
-
+	// self.res.on('close', () => console.log('Client is closed'));
 }
