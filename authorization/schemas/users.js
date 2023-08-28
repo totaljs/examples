fe341d6b588f7a9a7d8fcd538f931e41b8bfbc80
Kidchris <christianjosaphat0@gmail.com>
2023-08-28 14:23:52 +0000

    update  codes for authrization and assertion

--- authorization/schemas/users.js
+++ authorization/schemas/users.js
@@ -4,36 +4,40 @@ NEWSCHEMA('Users', function(schema) {
 	schema.define('password', 'String(30)', true);
 
 	// Performs login
-	schema.addWorkflow('login', function($, model) {
+	schema.action('login', {
+		name: 'Login',
+		action: function($, model){
+			var builder = NOSQL('users').one();
+			builder.where('email', model.email);
+			builder.where('password', model.password);
+			builder.callback(function(err, user) {
 
-		var builder = NOSQL('users').one();
-		builder.where('email', model.email);
-		builder.where('password', model.password);
-		builder.callback(function(err, user) {
-
-			if (!user) {
-				$.invalid('error-users-404');
-				return;
-			}
-
-			// Creates a cookie and session item
-			MAIN.session.authcookie($, UID(), user.id, '3 days');
-
-			// Writes audit
-			$.audit(user.id + ': ' + user.name);
-			$.success();
-		});
+				if (!user) {
+					$.invalid('error-users-404');
+					return;
+				}
+	
+				// Creates a cookie and session item
+				MAIN.session.authcookie($, UID(), user.id, '3 days');
+	
+				// Writes audit
+				$.audit(user.id + ': ' + user.name);
+				$.success();
+			});
+		}
 	});
 
 	// Performs logout
-	schema.addWorkflow('logout', function($) {
+	schema.action('logout', {
+		name: 'Logout',
+		action: function($) {
 
 		// Removes session
 		MAIN.session.logout($);
 
 		// Performs a redirect
 		$.redirect('/');
-
+		}
 	});
 
 });
