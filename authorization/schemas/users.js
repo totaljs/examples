0ff2c971a8564a2c4985503f2999ac2c581cc740
Louis Bertson <louisbertson@gmail.com>
2023-09-26 22:34:55 +0000

    Updated actions

--- authorization/schemas/users.js
+++ authorization/schemas/users.js
@@ -3,37 +3,40 @@ NEWSCHEMA('Users', function(schema) {
 	schema.define('email', 'Email', true);
 	schema.define('password', 'String(30)', true);
 
-	// Performs login
-	schema.addWorkflow('login', function($, model) {
+	
+	schema.action('login', {
+		name: 'Performs login',
+		action: function($, model) {
 
-		var builder = NOSQL('users').one();
-		builder.where('email', model.email);
-		builder.where('password', model.password);
-		builder.callback(function(err, user) {
-
-			if (!user) {
-				$.invalid('error-users-404');
-				return;
-			}
-
-			// Creates a cookie and session item
-			MAIN.session.authcookie($, UID(), user.id, '3 days');
-
-			// Writes audit
-			$.audit(user.id + ': ' + user.name);
-			$.success();
-		});
+			var builder = NOSQL('users').one();
+			builder.where('email', model.email);
+			builder.where('password', model.password);
+			builder.callback(function(err, user) {
+	
+				if (!user) {
+					$.invalid('error-users-404');
+					return;
+				}
+	
+				// Creates a cookie and session item
+				MAIN.session.authcookie($, UID(), user.id, '3 days');
+	
+				// Writes audit
+				$.audit(user.id + ': ' + user.name);
+				$.success();
+			});
+		}
 	});
 
-	// Performs logout
-	schema.addWorkflow('logout', function($) {
+	schema.action('logout', {
+		name: 'Performs logout',
+		action: function($) {
+			// Removes session
+			MAIN.session.logout($);
 
-		// Removes session
-		MAIN.session.logout($);
+			// Performs a redirect
+			$.redirect('/');
 
-		// Performs a redirect
-		$.redirect('/');
-
+		}
 	});
-
 });
--- threads/threads/products/schemas/products.js
+++ threads/threads/products/schemas/products.js
@@ -1,8 +1,11 @@
 NEWSCHEMA('Products', function(schema) {
 
-	schema.setQuery(function($) {
-		// FUNC.TESTDB is defined in /definitions/db.js
-		$.callback(FUNC.randomdata('products'));
+	schema.action('query', {
+		name: 'Query products list',
+		action: function($) {
+			// FUNC.TESTDB is defined in /definitions/db.js
+			$.callback(FUNC.randomdata('products'));
+		}
 	});
 
 });
